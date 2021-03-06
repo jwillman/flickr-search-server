f414ee0771d505e19082e8701463dd4ada7cb69f
Jani Willman <jani@willman.dev>
2021-03-06 17:26:33 +0200

    Flickr api queries must stay under 3600 requests per hour

--- FlickrSearch/index.js
+++ FlickrSearch/index.js
@@ -1,18 +1,42 @@
 const axios = require("axios");
+const NodeCache = require("node-cache");
+const fnfCache = new NodeCache();
+
+// TODO error handling
 
 module.exports = async function (context, req) {
     context.log("JavaScript HTTP trigger function processed a request.");
 
+    // TODO cache results
     const searchstring =
         req.query.searchstring || (req.body && req.body.searchstring);
     const results = req.query.results || (req.body && req.body.results);
 
-    // TODO stay under 3600 queries per hour
-    // TODO cache results
-    // TODO error handling
+    // Stay under 3600 queries per hour
+    let queryCount = fnfCache.get("queryCount");
+    if (queryCount == null) queryCount = 1;
+    fnfCache.set("queryCount", queryCount + 1);
+
+    let queriesStartTime = fnfCache.get("queriesStartTime");
+    if (queriesStartTime) {
+        const milliseconds = Math.abs(Date.now() - queriesStartTime);
+        const hours = milliseconds / 36e5;
+        if (queryCount > 3600 && hours < 1) {
+            context.res = {
+                body: {
+                    error: "Maximum queries per hour reached",
+                },
+            };
+        }
+        if (hours > 1) {
+            fnfCache.set("queryCount", 0);
+            fnfCache.set("queriesStartTime", Date.now());
+        }
+    } else {
+        fnfCache.set("queriesStartTime", Date.now());
+    }
 
     const url = `http://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${process.env.FLICKR_API_KEY}&text=${searchstring}&format=json&nojsoncallback=1`;
-
     const response = await axios.get(url);
 
     // Url format: https://live.staticflickr.com/{server-id}/{id}_{secret}.jpg
