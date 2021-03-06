139ce3d760be263900c00474a03c4ffcaa344f63
Jani Willman <jani@willman.dev>
2021-03-06 16:37:11 +0200

    Return the queried amount of photo urls

--- FlickrSearch/index.js
+++ FlickrSearch/index.js
@@ -9,26 +9,25 @@ module.exports = async function (context, req) {
 
     // TODO stay under 3600 queries per hour
     // TODO cache results
+    // TODO error handling
 
     const url = `http://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${process.env.FLICKR_API_KEY}&text=${searchstring}&format=json&nojsoncallback=1`;
 
-    console.log(url);
-
     const response = await axios.get(url);
 
-    console.log(response);
-
-    let server_id = response.data.photos.photo[1].server;
-    let id = response.data.photos.photo[1].id;
-    let secret = response.data.photos.photo[1].secret;
-
-    // https://live.staticflickr.com/{server-id}/{id}_{secret}.jpg
+    // Url format: https://live.staticflickr.com/{server-id}/{id}_{secret}.jpg
+    var photoUrls = [];
+    for (let step = 0; step < results; step++) {
+        let server_id = response.data.photos.photo[step].server;
+        let id = response.data.photos.photo[step].id;
+        let secret = response.data.photos.photo[step].secret;
+        let url = `https://live.staticflickr.com/${server_id}/${id}_${secret}.jpg`;
+        photoUrls.push(url);
+    }
 
     context.res = {
         body: {
-            photoUrls: [
-                `https://live.staticflickr.com/${server_id}/${id}_${secret}.jpg`,
-            ],
+            photoUrls: photoUrls,
         },
         headers: {
             "Content-Type": "application/json",
