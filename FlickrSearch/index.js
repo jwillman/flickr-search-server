2fe5d4c85f6f1bc1dc539bd057b573d9e6925aaa
Jani Willman <jani@willman.dev>
2021-03-07 12:51:16 +0200

    Added query param offset

--- FlickrSearch/index.js
+++ FlickrSearch/index.js
@@ -8,45 +8,30 @@ module.exports = async function (context, req) {
     context.log("JavaScript HTTP trigger function processed a request.");
 
     // TODO cache results
-    const searchstring =
-        req.query.searchstring || (req.body && req.body.searchstring);
-    const results = req.query.results || (req.body && req.body.results);
+    const searchstring = req.query.searchstring || "";
+    const results = parseInt(req.query.results) || 5;
+    const offset = parseInt(req.query.offset) || 0;
 
     // Stay under 3600 queries per hour
-    let queryCount = fnfCache.get("queryCount");
-    if (queryCount == null) queryCount = 1;
-    fnfCache.set("queryCount", queryCount + 1);
-
-    let queriesStartTime = fnfCache.get("queriesStartTime");
-    if (queriesStartTime) {
-        const milliseconds = Math.abs(Date.now() - queriesStartTime);
-        const hours = milliseconds / 36e5;
-        if (queryCount > 3600 && hours < 1) {
-            context.res = {
-                body: {
-                    error: "Maximum queries per hour reached",
-                },
-            };
-        }
-        if (hours > 1) {
-            fnfCache.set("queryCount", 0);
-            fnfCache.set("queriesStartTime", Date.now());
-        }
-    } else {
-        fnfCache.set("queriesStartTime", Date.now());
+    if (MaxQueryLimitReached(3600)) {
+        context.res = {
+            body: {
+                error: "Maximum queries per hour reached",
+            },
+        };
+        return;
     }
 
-    const url = `http://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${process.env.FLICKR_API_KEY}&text=${searchstring}&format=json&nojsoncallback=1`;
-    const response = await axios.get(url);
-
-    // Url format: https://live.staticflickr.com/{server-id}/{id}_{secret}.jpg
-    var photoUrls = [];
-    for (let step = 0; step < results; step++) {
-        let server_id = response.data.photos.photo[step].server;
-        let id = response.data.photos.photo[step].id;
-        let secret = response.data.photos.photo[step].secret;
-        let url = `https://live.staticflickr.com/${server_id}/${id}_${secret}.jpg`;
-        photoUrls.push(url);
+    try {
+        var photoUrls = await getPhotoUrls(
+            context,
+            process.env.FLICKR_API_KEY,
+            searchstring,
+            results,
+            offset
+        );
+    } catch (error) {
+        return;
     }
 
     context.res = {
@@ -58,3 +43,64 @@ module.exports = async function (context, req) {
         },
     };
 };
+
+async function getPhotoUrls(
+    context,
+    flickrApiKey,
+    searchstring,
+    results,
+    offset
+) {
+    // Url format: https://live.staticflickr.com/{server-id}/{id}_{secret}.jpg
+    const page = 1;
+    const perPage = 100;
+    const url = `http://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${flickrApiKey}&text=${searchstring}&format=json&nojsoncallback=1&page=${page}&per_page=${perPage}`;
+    const response = await axios.get(url);
+    var photoUrls = [];
+
+    if (response.data.stat === "fail") {
+        context.res = {
+            body: {
+                error: response.data.stat.message,
+            },
+        };
+        throw "Flickr api error";
+    }
+
+    if (response.data.photos != null) {
+        response.data.photos.photo.forEach((element) =>
+            photoUrls.push(
+                `https://live.staticflickr.com/${element.server}/${element.id}_${element.secret}.jpg`
+            )
+        );
+    }
+
+    if (results > perPage || results + offset > perPage) {
+        // TODO get another page
+        return photoUrls;
+    }
+
+    return photoUrls.slice(offset, results + offset);
+}
+
+function MaxQueryLimitReached(maxQueryAmount) {
+    let queryCount = fnfCache.get("queryCount");
+    if (queryCount == null) queryCount = 1;
+    fnfCache.set("queryCount", queryCount + 1);
+
+    let queriesStartTime = fnfCache.get("queriesStartTime");
+    if (queriesStartTime) {
+        const milliseconds = Math.abs(Date.now() - queriesStartTime);
+        const hours = milliseconds / 36e5;
+        if (queryCount > maxQueryAmount && hours < 1) {
+            return true;
+        }
+        if (hours > 1) {
+            fnfCache.set("queryCount", 0);
+            fnfCache.set("queriesStartTime", Date.now());
+        }
+    } else {
+        fnfCache.set("queriesStartTime", Date.now());
+    }
+    return false;
+}
